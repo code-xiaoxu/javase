07775bc0a8991fd57105e8d31bae05e1192082b3
xiaoxu <xuxi4077@outlook.com>
2024-07-31 15:54:34 +0800

    2 commit

--- src/main/java/com/xuxi/test/Test1.java
+++ src/main/java/com/xuxi/test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println();
+        System.out.println("hello git");
     }
 }
